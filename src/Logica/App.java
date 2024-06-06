be28919a8afef45ea0ca05ed279d0ff8f08255d0
Amanda Galán <ammandiitaa13@gmail.com>
2024-06-06 18:46:16 +0200

    Soy una cruck y Tobal y Lusito cuando la boda

--- src/Logica/App.java
+++ src/Logica/App.java
@@ -1,4 +1,4 @@
-package logica;
+/*package logica;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -71,3 +71,4 @@ public class App {
     }
 
 }
+*/
---
+++ src/Logica/Main.java
@@ -0,0 +1,11 @@
+package Logica;
+import DAO.*;
+import Frames.*;
+import Interfaces.CRUD;
+public class Main {
+    public static void main(String args[]) throws Exception {
+        Login login = new Login();
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);
+    }
+}
